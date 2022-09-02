1b145eef24c75d74825fcc53fd8fae13f8034a15
justinkang221 <jkang@Justins-MacBook-Pro.local>
2022-09-02 14:19:13 -0700

    Modifying synthetic data plotting

--- compute_point.py
+++ compute_point.py
@@ -2,16 +2,19 @@ import numpy as np
 from src.qspright.inputsignal import Signal
 
 
-def compute_point(sparsity, spright):
-    q = 4
-    n = 10
-    a = 1
-    b = 10
-    noise_sd = 0
+def compute_point_qspright(sparsity, spright, config):
+    q = config["q"]
+    n = config["n"]
+    a = config["a"]
+    b = config["b"]
+    noise_sd = config["noise_sd"]
+    n_points = config["n_points"]
     n_fail = 0
-    n_points = 100
+    n_samples = 0
     for j in range(n_points):
         signal = Signal(n=n, q=q, sparsity=sparsity, a=a, b=b, noise_sd=noise_sd)
         gwht, (n_used, n_used_unique), peeled = spright.transform(signal, verbose=False, report=True)
         n_fail += (np.sum(np.abs(signal._signal_w - gwht) ** 2) / np.sum(np.abs(signal._signal_w) ** 2)) > 0.1
-    return n_fail/n_points
+        n_samples += n_used_unique
+    return n_fail/n_points, n_samples/(n_points*(q ** n))
+
--- synthetic_data_plots.py
+++ synthetic_data_plots.py
@@ -3,7 +3,7 @@ from multiprocessing import Pool
 import sys
 from src.qspright.qspright_nso import QSPRIGHT
 from itertools import repeat
-from compute_point import compute_point
+from compute_point import compute_point_qspright
 import matplotlib.pyplot as plt
 sys.path.append("./src/qspright/")
 np.random.seed(10)
@@ -14,12 +14,23 @@ if __name__ == '__main__':
         delays_method="identity",
         reconstruct_method="noiseless",
         num_subsample=2,
-        b=4
+        b=3
     )
-    sparsities = [20*(i+1) for i in range(15)]
+    config = {
+        "q": 3,
+        "n": 6,
+        "a": 1,
+        "b": 10,
+        "noise_sd": 0,
+        "n_points": 300
+    }
+    sparsities = [1 + 4*i for i in range(15)]
     p = Pool()
-    result = p.starmap(compute_point, zip(sparsities, repeat(spright)))
-    plt.plot(sparsities, result)
+    result = p.starmap(compute_point_qspright, zip(sparsities, repeat(spright), repeat(config)))
+    result = np.array(result)
+    plt.plot(sparsities, result[:, 0])
     plt.xlabel("Sparsity Level")
     plt.ylabel("Fraction of Failures")
     plt.show()
+    print("Sample Rate:")
+    print(np.mean(result[:, 1]))
