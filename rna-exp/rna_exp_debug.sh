3794870a59fed8a81d63f6d41235cd66a965d881
Yigit Efe Erginbas <erginbas@berkeley.edu>
2022-09-17 17:34:39 -0700

    correction to slurm

--- rna-exp/rna_exp_debug.sh
+++ rna-exp/rna_exp_debug.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=rna_exp
 #SBATCH --account=fc_basics
 #SBATCH --partition=savio
-#SBATCH --time=04:00:00
+#SBATCH --time=00:10:00
 #SBATCH --output=slurm_outputs/rna_exp_debug_%j.out
 #SBATCH --error=slurm_outputs/rna_exp_debug_%j.err
 #SBATCH --qos=savio_debug
