5f0891197f8f7ee8991b0615c661e4b91765bd1c
Yigit Efe Erginbas <erginbas@berkeley.edu>
2022-09-17 20:00:44 -0700

    no message

--- rna-exp/rna_exp_debug.sh
+++ rna-exp/rna_exp_debug.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:10:00
 #SBATCH --output=slurm_outputs/rna_exp_debug_%j.out
 module load python
-source activate generalized-wht
+source activate qspright
 ipython -V
 which python
 ipython qspright-sample-vs-estimation-accuracy.ipynb
