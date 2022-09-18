72e04a8d49c5f36249c5bdbefd02af0bd2453507
YigitEfe Erginbas <erginbas@ln002.brc>
2022-09-17 19:37:36 -0700

    xx

--- rna-exp/rna_exp_debug.sh
+++ rna-exp/rna_exp_debug.sh
@@ -4,9 +4,13 @@
 #SBATCH --partition=savio
 #SBATCH --time=00:10:00
 #SBATCH --output=slurm_outputs/rna_exp_debug_%j.out
-#SBATCH --error=slurm_outputs/rna_exp_debug_%j.err
 #SBATCH --qos=savio_debug
 module load python
 source activate generalized-wht
 jupyter nbconvert --to script qspright-sample-vs-estimation-accuracy.ipynb
-python qspright-sample-vs-estimation-accuracy.py
+echo ----------
+python -V
+which python
+which python3
+conda info --envs
+ipython qspright-sample-vs-estimation-accuracy.py
