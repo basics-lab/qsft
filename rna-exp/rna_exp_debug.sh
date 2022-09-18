1229c9fc4c6c75b9ffcf2d7389100eb6e19f86f9
Yigit Efe Erginbas <erginbas@berkeley.edu>
2022-09-17 19:52:05 -0700

    xx

--- rna-exp/rna_exp_debug.sh
+++ rna-exp/rna_exp_debug.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=rna_exp
 #SBATCH --account=fc_basics
-#SBATCH --partition=savio
+#SBATCH --partition=savio2
 #SBATCH --time=00:10:00
 #SBATCH --output=slurm_outputs/rna_exp_debug_%j.out
-#SBATCH --qos=savio_debug
 module load python
 source activate generalized-wht
 ipython -V
