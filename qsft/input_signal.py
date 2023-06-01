da147d6701e1cb40fe60eba188deb9369884b786
Landon Butler <48364057+landonbutler@users.noreply.github.com>
2023-06-01 16:11:38 -0700

    Created slot allocation game with custom social welfare function

--- qsft/input_signal.py
+++ qsft/input_signal.py
@@ -2,7 +2,7 @@
 A shell Class for common interface to an input signal. This class should be extended when implemented
 """
 import numpy as np
-from qsft.utils import gwht_tensored, igwht_tensored, save_data, load_data
+from qsft.qsft.utils import gwht_tensored, igwht_tensored, save_data, load_data
 from pathlib import Path
 
 
